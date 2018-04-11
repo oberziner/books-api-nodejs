7af87dad886993621245346eec7791c84d63a2cc
Augusto Oberziner <aoberzin@laaoberzin.gap.com>
2018-04-11 10:19:06 -0300

    Reading books from database

--- config/datasource.js
+++ config/datasource.js
@@ -4,26 +4,41 @@ import path from 'path';
 
 let database = null;
 
+const loadModels = (sequelize) => {
+  const dir = path.join(__dirname, '../models');
+  let models = [];
+
+  fs.readdirSync(dir).forEach(file => {
+    const modelDir = path.join(dir, file),
+      model = sequelize.import(modelDir);
+    models[model.name] = model;
+  });
+
+  return models;
+}
+
 export default (app) => {
-    if (!database) {
-        const config = app.config,
-            sequelize = new Sequelize(
-                config.database,
-                config.username,
-                config.password,
-                config.params
-            );
-        database = {
-            sequelize,
-            Sequelize,
-            models: {}
-        };
+  if (!database) {
+    const config = app.config,
+      sequelize = new Sequelize(
+        config.database,
+        config.username,
+        config.password,
+        config.params
+      );
+    database = {
+      sequelize,
+      Sequelize,
+      models: {}
+    };
 
-        sequelize.sync().done(() => {
-            return database;
-        });
+    database.models = loadModels(sequelize);
 
-    }
+    sequelize.sync().done(() => {
+      return database;
+    });
 
-    return database;
+  }
+
+  return database;
 }
--- test/integration/app.js
+++ test/integration/app.js
@@ -1,19 +1,27 @@
 describe('Routes Books', () => {
-    const defaultBook = {
-        id: 1,
-        name: 'Default Book'
+  const Books = app.datasource.models.Books,
+    defaultBook = {
+      id: 1,
+      name: 'Default Book'
     };
 
-    describe('Route GET /books', () => {
-        it('should return a list of books', done => {
-            request
-                .get('/books')
-                .end((err, res) => {
-                    expect(res.body[0].id).to.be.eql(defaultBook.id);
-                    expect(res.body[0].name).to.be.eql(defaultBook.name);
+  beforeEach(done => {
+    Books
+      .destroy({where: {}})
+      .then(() => Books.create(defaultBook))
+      .then(() => done());
+  });
 
-                    done(err);
-                });
+  describe('Route GET /books', () => {
+    it('should return a list of books', done => {
+      request
+        .get('/books')
+        .end((err, res) => {
+          expect(res.body[0].id).to.be.eql(defaultBook.id);
+          expect(res.body[0].name).to.be.eql(defaultBook.name);
+
+          done(err);
         });
     });
+  });
 })

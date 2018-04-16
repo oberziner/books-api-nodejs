7c01e84fa9122904944d8c05fb1e36a62126936c
Augusto Oberziner <augustoberziner@gmail.com>
2018-04-15 22:48:52 -0300

    Implemented getAll on BooksController

--- test/unit/controllers/books.js
+++ test/unit/controllers/books.js
@@ -1,7 +1,24 @@
+import BooksController from '../../../controllers/books';
+
 describe('Controllers: Books', () => {
   describe('Get all books: getAll()', () => {
     it('should return a list of books', () => {
+      const Books = {
+        findAll: td.function(),
+      };
 
+      const expectedResponse = [{
+        id: 1,
+        name: 'Test Book',
+        created_at: '2016-08-06T23:55:36.692Z',
+        updated_at: '2016-08-06T23:55:36.692Z',
+      }];
+
+      td.when(Books.findAll({})).thenResolve(expectedResponse);
+
+      const booksController = new BooksController(Books);
+      return booksController.getAll()
+        .then(response => expect(response.data).to.be.eql(expectedResponse));
     });
   });
 });

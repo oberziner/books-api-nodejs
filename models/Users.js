73336db83c10611bcf608ab1cf90e0bbd28060d5
Augusto Oberziner <augustoberziner@gmail.com>
2018-04-18 13:10:13 -0300

    Creating Users model

---
+++ models/Users.js
@@ -0,0 +1,40 @@
+import bcrypt from 'bcrypt';
+
+export default (sequelize, DataType) => sequelize.define('Users', {
+  id: {
+    type: DataType.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataType.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  email: {
+    type: DataType.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  password: {
+    type: DataType.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+}, {
+  hooks: {
+    beforeCreate: (user) => {
+      const salt = bcrypt.genSaltSync();
+      user.set('password', bcrypt.hashSync(user.password, salt));
+    },
+  },
+  classMethods: {
+    isPassword: (encodedPassword, password) => bcrypt.compare(password, encodedPassword),
+  },
+});

3d02d6b2f9b8240bd772f7e036bea0fc9dd4290e
Augusto Oberziner <augustoberziner@gmail.com>
2018-04-18 19:05:40 -0300

    Created users CRUD routes

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ import bodyParser from 'body-parser';
 import config from './config/config';
 import datasource from './config/datasource';
 import booksRouter from './routes/books';
+import usersRouter from './routes/users';
 
 const app = express();
 
 app.config = config;
 app.datasource = datasource(app);
+
 app.set('port', 7000);
 app.use(bodyParser.json());
 
-const { Books } = app.datasource.models;
-booksRouter(app, Books);
+booksRouter(app);
+usersRouter(app);
 
 export default app;
---
+++ test/integration/routes/users.js
@@ -0,0 +1,99 @@
+describe('Routes Users', () => {
+  const { Users } = app.datasource.models;
+  const defaultUser = {
+    id: 1,
+    name: 'Default User',
+    email: 'test@mail.com',
+    password: 'test',
+  };
+
+  beforeEach((done) => {
+    Users
+      .destroy({ where: {} })
+      .then(() => Users.create(defaultUser))
+      .then(() => done());
+  });
+
+  describe('Route GET /users', () => {
+    it('should return a list of users', (done) => {
+      request
+        .get('/users')
+        .end((err, res) => {
+          expect(res.body[0].id).to.be.eql(defaultUser.id);
+          expect(res.body[0].name).to.be.eql(defaultUser.name);
+          expect(res.body[0].email).to.be.eql(defaultUser.email);
+
+          done(err);
+        });
+    });
+  });
+
+  describe('Route GET /users/{id}', () => {
+    it('should return a a users', (done) => {
+      request
+        .get('/users/1')
+        .end((err, res) => {
+          expect(res.body.id).to.be.eql(defaultUser.id);
+          expect(res.body.name).to.be.eql(defaultUser.name);
+          expect(res.body.email).to.be.eql(defaultUser.email);
+
+          done(err);
+        });
+    });
+  });
+
+  describe('Route POST /users', () => {
+    it('should create a user', (done) => {
+      const newUser = {
+        id: 2,
+        name: 'new user',
+        email: 'test@mail.com',
+        password: 'test',
+      };
+
+      request
+        .post('/users')
+        .send(newUser)
+        .end((err, res) => {
+          expect(res.statusCode).to.be.eql(201);
+          expect(res.body.id).to.be.eql(newUser.id);
+          expect(res.body.name).to.be.eql(newUser.name);
+          expect(res.body.email).to.be.eql(newUser.email);
+
+          done(err);
+        });
+    });
+  });
+
+  describe('Route PUT /users/{id}', () => {
+    it('should update a user', (done) => {
+      const updatedUser = {
+        id: 1,
+        name: 'updated user',
+        email: 'test@mail.com',
+      };
+
+      request
+        .put('/users/1')
+        .send(updatedUser)
+        .end((err, res) => {
+          expect(res.body).to.be.eql([1]);
+
+          done(err);
+        });
+    });
+  });
+
+  describe('Route DELETE /users/{id}', () => {
+    it('should delete a user', (done) => {
+      request
+        .delete('/users/1')
+        .end((err, res) => {
+          expect(res.statusCode).to.be.eql(204);
+
+          done(err);
+        });
+    });
+  });
+});
+
